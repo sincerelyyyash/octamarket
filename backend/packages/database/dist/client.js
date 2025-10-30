9e1c9c4d6380950554d7b9f868ef7a39bd0b6970
Ashish Mohapatra <ashishmohapatra240@gmail.com>
2025-10-30 18:15:44 +0530

    feat(db): add Prisma migrations and update database package; refresh lockfile

--- backend/packages/database/dist/client.js
+++ backend/packages/database/dist/client.js
@@ -6192,7 +6192,7 @@ var require_client = __commonJS({
           "value": "prisma-client-js"
         },
         "output": {
-          "value": "/Users/yash/DevProjects/s30/opinion-markets/backend/packages/database/generated/client",
+          "value": "D:\\Work\\octamarket\\backend\\packages\\database\\generated\\client",
           "fromEnvVar": null
         },
         "config": {
@@ -6201,12 +6201,12 @@ var require_client = __commonJS({
         "binaryTargets": [
           {
             "fromEnvVar": null,
-            "value": "darwin-arm64",
+            "value": "windows",
             "native": true
           }
         ],
         "previewFeatures": [],
-        "sourceFilePath": "/Users/yash/DevProjects/s30/opinion-markets/backend/packages/database/prisma/schema.prisma",
+        "sourceFilePath": "D:\\Work\\octamarket\\backend\\packages\\database\\prisma\\schema.prisma",
         "isCustomOutput": true
       },
       "relativeEnvPaths": {
@@ -6257,8 +6257,8 @@ var require_client = __commonJS({
     var PrismaClient2 = getPrismaClient2(config);
     exports2.PrismaClient = PrismaClient2;
     Object.assign(exports2, Prisma);
-    path.join(__dirname, "libquery_engine-darwin-arm64.dylib.node");
-    path.join(process.cwd(), "generated/client/libquery_engine-darwin-arm64.dylib.node");
+    path.join(__dirname, "query_engine-windows.dll.node");
+    path.join(process.cwd(), "generated/client/query_engine-windows.dll.node");
     path.join(__dirname, "schema.prisma");
     path.join(process.cwd(), "generated/client/schema.prisma");
   }
